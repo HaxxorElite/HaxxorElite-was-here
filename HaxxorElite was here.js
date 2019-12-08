ad045da04fabc6516f531d55fe9142f4b3e4f7f7
HaxxorElite <58654170+HaxxorElite@users.noreply.github.com>
2019-12-08 11:06:47 +0100

    Add files via upload

---
+++ HaxxorElite was here.js
@@ -0,0 +1,8 @@
+var Protobuf = require('protobufjs');
+
+var builder = Protobuf.newBuilder({"convertFieldsToCamelCase": true});
+Protobuf.loadProtoFile(__dirname + '/protos/base_gcmessages.proto', builder);
+Protobuf.loadProtoFile(__dirname + '/protos/gcsdk_gcmessages.proto', builder);
+Protobuf.loadProtoFile(__dirname + '/protos/cstrike15_gcmessages.proto', builder);
+
+module.exports = builder.build(); HaxxorElite was here
